0036bc916d9238fa788cb854495332b2dfa1318c
Ahmad Raza <ahmadraza_@outlook.com>
2019-09-26 18:23:07 +0500

    Added a function in Flight model to check if a flight is valid or not

--- flights/models.py
+++ flights/models.py
@@ -9,13 +9,15 @@ class Airport(models.Model):
 	def __str__(self):
 		return f"{self.city} ({self.code})"
 
+
 class Flight(models.Model):
 	origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
 	destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
 	duration = models.IntegerField()
 
-	def __str__(self):
-		return f"{self.id} - {self.origin} to {self.destination}"
+	def is_valid_flight(self):
+        return (self.origin != self.destination) and (self.duration >= 0)
+
 
 class Passenger(models.Model):
 	first_name = models.CharField(max_length=25)
