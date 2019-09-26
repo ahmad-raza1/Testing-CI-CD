79d47125bd92d67476bfb5237ca902f8b4e4e9c0
Ahmad Raza <ahmadraza_@outlook.com>
2019-09-26 18:28:20 +0500

    Updated

--- flights/models.py
+++ flights/models.py
@@ -18,6 +18,9 @@ class Flight(models.Model):
 	def is_valid_flight(self):
         return (self.origin != self.destination) and (self.duration >= 0)
 
+	def __str__(self):
+		return f"{self.id} - {self.origin} to {self.destination}"
+
 
 class Passenger(models.Model):
 	first_name = models.CharField(max_length=25)
