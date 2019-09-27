84fd82253c0d3791377d7d92b98d57ee7d6978c6
Ahmad Raza <54940472+ahmad-raza1@users.noreply.github.com>
2019-09-27 22:27:39 +0500

    Customized admin interface.

--- flights/admin.py
+++ flights/admin.py
@@ -7,3 +7,14 @@ from .models import Airport, Flight, Passenger
 admin.site.register(Airport)
 admin.site.register(Flight)
 admin.site.register(Passenger)
+
+# Customize admin interface.
+class PassengerInline(admin.StackedInline):
+    model = Passenger.flights.through
+    extra = 1
+
+class FlightAdmin(admin.ModelAdmin):
+    inlines = [PassengerInline]
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights",)
