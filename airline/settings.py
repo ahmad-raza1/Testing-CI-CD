a40659aca763f71e14347586c38d9bd9a4fac766
Ahmad Raza <ahmadraza_@outlook.com>
2019-09-27 17:35:22 +0500

    Updated

--- airline/settings.py
+++ airline/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from decouple import config, Csv
 import dj_database_url
-#import django_heroku
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -138,4 +138,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Activate django-heroku.
-#django_heroku.settings(locals())
+if HEROKU_ENV is True:
+    django_heroku.settings(locals())
