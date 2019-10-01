baa7857afaa000f8cc0df1a870d83d1c5d960862
Ahmad Raza <ahmadraza_@outlook.com>
2019-10-01 16:37:02 +0500

    Updated

--- airline/settings.py
+++ airline/settings.py
@@ -120,10 +120,12 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+"""
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+"""
 
 # Activate django-heroku.
 if 'HEROKU_ENV' in os.environ:
